02e1c9f2c9420b1bea9381f610e1a0d3f8551337
Raphael Negrisoli Batista <raphaelnegrisoli@gmail.com>
2013-05-08 20:21:59 -0300

    Improve array files.

--- tools/install.sh
+++ tools/install.sh
@@ -3,12 +3,7 @@
 bkp_dir = "$HOME/.dotfiles_old"
 target_dir = "$HOME/.dotfiles"
 
-files[0] = ".vimrc"
-files[1] = ".vim.after"
-files[2] = ".bashrc"
-files[3] = ".profile"
-files[4] = ".vim"
-files[5] = ".zshrc"
+files=(".vimrc" ".vim.after" ".bashrc" ".profile" ".vim" ".zshrc")
 
 echo "\033[0;34nCloning dotfiles...\033[0m"
 hash git >/dev/null && /usr/bin/env git clone git@github.com:rbatista/dotfiles.git $target_dir || {
@@ -26,14 +21,14 @@ echo "\033[0;34nand updating...\033[0m"
 
 echo "\033[0;34nLooking for existing configs...\033[0m"
 echo "\033[0;34nYour configurations will be backuped in ~/.dotfiles_old/\033[0m"
-mkdir ~/.dotfiles_old
+mkdir $HOME/$bkp_dir
 
 for i in $files
 do
   if [ -f $HOME/$i] || [ -h $HOME/$i]
   then
     echo "\033[0;34nBackuping $HOME/$i into $bkp_dir/$i...\033[0m"
-    mv -r $HOME/$i bkp_dir/$i
+    mv -r $HOME/$i $bkp_dir/$i
     echo "\033[0;34nLinking $i into your home...\033[0m"
     ln -s $target_dir/$i $HOME/$i
   fi
